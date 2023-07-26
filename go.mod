f4c3c84ce3f7c3eb1064853b6c326c89650eeaab
René Post <git@xpt.nl>
2023-07-26 12:23:38 +0200

    Completely refactor the package

--- go.mod
+++ go.mod
@@ -3,23 +3,23 @@ module github.com/reactivego/vibrant
 go 1.20
 
 require (
-	gioui.org v0.0.0-20230526230622-e3ef98dda382
-	github.com/reactivego/gio v0.0.1
-	github.com/reactivego/x v0.1.1
+	gioui.org v0.1.0
+	github.com/reactivego/x v0.1.7
 )
 
 require (
 	eliasnaur.com/font v0.0.0-20230308162249-dd43949cb42d // indirect
 	gioui.org/cpu v0.0.0-20220412190645-f1e9e8c3b1f7 // indirect
 	gioui.org/shader v1.0.6 // indirect
-	github.com/go-text/typesetting v0.0.0-20230504123538-7c21ee424d89 // indirect
+	github.com/go-text/typesetting v0.0.0-20230717141307-09c70c30a055 // indirect
+	github.com/reactivego/gio v0.0.2
 	github.com/reactivego/ivg v0.0.9 // indirect
 	github.com/reactivego/scheduler v0.0.8 // indirect
 	github.com/reactivego/seen v0.0.1 // indirect
 	github.com/reactivego/svg v0.0.1 // indirect
-	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
-	golang.org/x/exp/shiny v0.0.0-20230522175609-2e198f4a06a1 // indirect
-	golang.org/x/image v0.7.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230725093048-515e97ebf090 // indirect
+	golang.org/x/exp/shiny v0.0.0-20230725093048-515e97ebf090 // indirect
+	golang.org/x/image v0.9.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 )
---
+++ vibrant.go
@@ -0,0 +1,111 @@
+package vibrant
+
+import (
+	"image/color"
+	"log"
+	"unsafe"
+
+	"gioui.org/app"
+	"gioui.org/f32"
+	"gioui.org/io/event"
+	"gioui.org/io/pointer"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+
+	"github.com/reactivego/gio"
+	"github.com/reactivego/x"
+)
+
+const kLogEvents = false
+
+func Backdrops(fill color.Color) x.Observable[layout.Widget] {
+	return x.Of(gio.Backdrop(fill))
+}
+
+func LinearGradients(stop1 f32.Point, color1 color.Color, stop2 f32.Point, color2 color.Color) x.Observable[layout.Widget] {
+	return x.Of(gio.LinearGradient(stop1, color1, stop2, color2))
+}
+
+type MsgOp struct{ Msg any }
+
+func (op MsgOp) Add(ops *op.Ops) {
+	defer clip.Rect{}.Push(ops).Pop()
+	pointer.InputOp{Tag: op}.Add(ops)
+}
+
+// Window handles the events of a single gioui app window.
+type Window struct {
+	*app.Window
+
+	msgOps chan MsgOp
+}
+
+func NewWindow(options ...app.Option) *Window {
+	return &Window{Window: app.NewWindow(options...), msgOps: make(chan MsgOp, 1)}
+}
+
+func (window *Window) MsgOps() x.Observable[MsgOp] {
+	return x.FromChan(window.msgOps)
+}
+
+func (window *Window) Layout(layers ...x.Observable[layout.Widget]) x.Subscription {
+	events := x.FromChan(window.Events()).Filter(func(next event.Event) bool {
+		if kLogEvents {
+			log.Printf("event: %[1]T %[1]v\n", next)
+		}
+		return next != nil
+	})
+
+	// Slow loading layers should not block the event loop.
+	blank := func(gtx layout.Context) layout.Dimensions {
+		return layout.Dimensions{Size: gtx.Constraints.Max}
+	}
+	for i := range layers {
+		layers[i] = layers[i].StartWith(blank)
+	}
+
+	// Whenever the layers change, invalidate the window.
+	invalidate := func(layers []layout.Widget) []layout.Widget {
+		window.Invalidate()
+		return layers
+	}
+
+	pairs := x.WithLatestFromPair(events, x.Map(x.Combine(layers...), invalidate).SubscribeOn(x.Goroutine))
+
+	var ops op.Ops
+	main := func(next x.Pair[event.Event, []layout.Widget], err error, done bool) {
+		switch {
+		case !done:
+			if frame, ok := next.First.(system.FrameEvent); ok {
+				gtx := layout.NewContext(&ops, frame)
+				for _, widget := range next.Second {
+					widget(gtx)
+				}
+				frame.Frame(gtx.Ops)
+				type internalOps struct {
+					version     int
+					data        []byte
+					refs        []interface{}
+					nextStateID int
+					multipOp    bool
+				}
+				for _, op := range (*internalOps)(unsafe.Pointer(&ops.Internal)).refs {
+					if msgop, matches := op.(MsgOp); matches {
+						window.msgOps <- msgop
+					}
+				}
+			}
+		case err != nil:
+			// log.Printf("error: %v\n", err)
+		default:
+			// log.Println("complete")
+			if window.msgOps != nil {
+				close(window.msgOps)
+				window.msgOps = nil
+			}
+		}
+	}
+	return pairs.Subscribe(main, x.NewScheduler())
+}
